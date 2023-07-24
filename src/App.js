499a4dde962ca05c06b0f7fb1937984b503e747e
Ray Badua <rayjonathanbadua@gmail.com>
2023-07-23 17:18:19 -0700

    initial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/weather/`)
+      .then((data) => data.json())
+      .then((data) => {
+        setState(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hi</h1>
+      <p>{state}</p>
+    </>
   );
 }
 
