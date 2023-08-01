ecb67a07a1ad7a630eeb8f9587d10897077477f4
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-01 14:14:06 -0700

    initial files, cra -> vite

--- client/src/services/getTides.js
+++
@@ -1,8 +0,0 @@
-export const getTides = async () => {
-  const response = await fetch("http://localhost:3001/api/water/tides").then(
-    (data) => data.json()
-  );
-  const tideData = response.predictions;
-
-  return tideData;
-};
--- server/controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const weatherRoutes = require("./weatherRoutes");
-const waterRoutes = require("./waterRoutes");
-
-router.use("/weather", weatherRoutes);
-router.use("/water", waterRoutes);
-
-module.exports = router;
